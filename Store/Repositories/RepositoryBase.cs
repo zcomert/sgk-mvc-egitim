1fa4c6ad9eaebb483a60081e6d9a99ed0d601bfc
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-11-24 15:40:14 +0300

    RepositoryBase has been defined.

---
+++ Store/Repositories/RepositoryBase.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq.Expressions;
+using Repositories.Contracts;
+
+namespace Repositories;
+
+public class RepositoryBase<T> : IRepositoryBase<T>
+where T : class
+{
+    private readonly RepositoryContext _context;
+
+    public RepositoryBase(RepositoryContext context)
+    {
+        _context = context;
+    }
+
+    public int Count => _context.Set<T>().Count();
+
+    public void Create(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T? Read(Expression<Func<T, bool>> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<T> ReadAll(Expression<Func<T, bool>> filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(T item)
+    {
+        throw new NotImplementedException();
+    }
+}
