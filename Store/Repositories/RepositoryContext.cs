40422f1f66f7702e6cf47d81459b47ad2f8fff0e
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-11-24 11:47:32 +0300

    Updated.

--- Store/Repositories/RepositoryContext.cs
+++ Store/Repositories/RepositoryContext.cs
@@ -11,7 +11,7 @@ public class RepositoryContext : DbContext
 
     }
     public DbSet<Product> Products { get; set; }
-    public DbSet<Product> Categories { get; set; }
+    public DbSet<Category> Categories { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
