1a88467760b6461ad7095b222331281821e82342
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-08 14:31:02 +0300

    Updated.

--- Store/StoreApp/Areas/Admin/Controllers/RoleController.cs
+++ Store/StoreApp/Areas/Admin/Controllers/RoleController.cs
@@ -1,9 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
 
 namespace StoreApp.Areas.Admin.Controllers;
 
 [Area("Admin")]
 public class RoleController : Controller
 {
-    
+    private readonly IServiceManager _manager;
+
+    public RoleController(IServiceManager manager)
+    {
+        _manager = manager;
+    }
+
+    public IActionResult Index()
+    {
+        var model = _manager
+            .AuthService
+            .GetAllRoles();
+        return View(model);
+    }
 }
