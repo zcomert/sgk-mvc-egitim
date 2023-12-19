cdcc75ca5c9bcf7adf0e8116456cf5ebef1be62c
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-19 13:55:53 +0300

    Static class has been updated.

--- api/StoreApi/Controllers/BooksFakeController.cs
+++ api/StoreApi/Controllers/BooksFakeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using StoreApi.InMemoryRepositories;
+using StoreApi.Models;
 
 namespace StoreApi.Controllers;
 
@@ -17,6 +18,56 @@ public class BooksFakeController : ControllerBase
     [HttpGet]
     public IActionResult GetAllBooks()
     {
-        return Ok(_fakeBookRepositoy.Books);
+        try
+        {
+            return Ok(_fakeBookRepositoy.Books);
+        }
+        catch (System.Exception)
+        {
+            throw;
+        }
+    }
+
+    [HttpGet("{id:int}")]
+    public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
+    {
+        try
+        {
+            return Ok(_fakeBookRepositoy.GetOne(id));
+        }
+        catch (System.Exception)
+        {
+            throw;
+        }
+    }
+
+    [HttpPost]
+    public IActionResult CreateOneBook([FromBody] Book book)
+    {
+        try
+        {
+            _fakeBookRepositoy.Add(book);
+            return NoContent();
+        }
+        catch (System.Exception)
+        {
+            throw;
+        }
+
+    }
+
+    [HttpPut("{id:int}")]
+    public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id,
+    [FromBody] Book book)
+    {
+        try
+        {
+            return Ok(_fakeBookRepositoy.UpdateBook(id, book));
+
+        }
+        catch (System.Exception)
+        {
+            throw;
+        }
     }
 }
