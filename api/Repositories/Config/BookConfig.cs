786b9c77e3ec799104ba9437b8f93b84496d00ff
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-20 12:07:09 +0300

    Repositories has been defined.

--- api/Repositories/Config/BookConfig.cs
+++ api/Repositories/Config/BookConfig.cs
@@ -11,12 +11,12 @@ public class BookConfig : IEntityTypeConfiguration<Book>
         // Fluent API
         builder.HasKey(b => b.Id); // Id - BookId
         builder.Property(b => b.Title).IsRequired();
-        builder.Property(b => b.Price).HasDefaultValue(10);
+
 
         builder.HasData(
-            new Book() { Id=1, Title="Mesnevi", Price = 300},    
-            new Book() { Id=2, Title="Devlet", Price = 250},
-            new Book() { Id=3, Title="Sefiller", Price = 350}
+            new Book() { Id = 1, Title = "Mesnevi", Price = 300 },
+            new Book() { Id = 2, Title = "Devlet", Price = 250 },
+            new Book() { Id = 3, Title = "Sefiller", Price = 350 }
         );
     }
 }
---
+++ api/StoreApi/Migrations/20231220085621_Init.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreApi.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Price", "Title" },
+                values: new object[] { 1, 300m, "Mesnevi" });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Price", "Title" },
+                values: new object[] { 2, 250m, "Devlet" });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Price", "Title" },
+                values: new object[] { 3, 350m, "Sefiller" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
