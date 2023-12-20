9c56a92969baf1d1ea58522d125754d648bcc0f4
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-20 11:32:47 +0300

    Repositories has been defined.

---
+++ api/Repositories/Config/BookConfig.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repositories.Config;
+
+public class BookConfig : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        // Fluent API
+        builder.HasKey(b => b.Id); // Id - BookId
+        builder.Property(b => b.Title).IsRequired();
+        builder.Property(b => b.Price).HasDefaultValue(10);
+
+        builder.HasData(
+            new Book() { Id=1, Title="Mesnevi", Price = 300},    
+            new Book() { Id=2, Title="Devlet", Price = 250},
+            new Book() { Id=3, Title="Devlet", Price = 250},
+        );
+    }
+}
