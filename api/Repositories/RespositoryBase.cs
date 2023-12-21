29d1fe94420fe88b8b2f7115d6c1d03a6398c673
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-21 14:28:08 +0300

    IRepositoryBase has been defined.

---
+++ api/Repositories/RespositoryBase.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq.Expressions;
+using Repositories.Contracts;
+
+namespace Repositories;
+
+public class RespositoryBase<T> : IRepositoryBase<T>
+    where T : class
+{
+    private readonly RepositoryContext _context;
+
+    public RespositoryBase(RepositoryContext context)
+    {
+        _context = context;
+    }
+
+    public void Create(T entity)
+    {
+        _context.Set<T>().Add(entity);
+        _context.SaveChanges();
+    }
+
+    public void Delete(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+        _context.SaveChanges();
+    }
+
+    public T? Read(Expression<Func<T, bool>> filter, bool trackChanges)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<T>? ReadAll(Expression<Func<T, bool>> filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
