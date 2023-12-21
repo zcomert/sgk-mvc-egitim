73433e2773f97e931ec4781ab3a9b6e1b89cedab
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-21 10:07:18 +0300

    Global Errorhandling.

---
+++ api/StoreApi/Infrastructure/Extensions/ExceptionMiddlewareExtension.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace StoreApi.Infrastructure.Extensions;
+
+public static class ExceptionMiddlewareExtension
+{
+    public static void ConfigureExceptionHandler(this WebApplication app)
+    {
+        app.UseExceptionHandler(appError =>
+        {
+            appError.Run(async context =>
+            {
+                context.Response.ContentType = "application/json";
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+            });
+        });
+    }
+}
---
+++ api/StoreApi/Infrastructure/Extensions/Extension.md
@@ -0,0 +1,4 @@
+1. Eklenti metotlar static sınıflara yazılır.
+2. Static sınıfların tüm üyeleri static olur.
+3. Genişletilecek tip this ile belirtilir. 
+4. 
