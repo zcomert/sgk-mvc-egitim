8e01976b9688bab80f97c0a755b60d3f3677a111
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-22 14:14:54 +0300

    Services project has been updated.

---
+++ api/Services/BookManager.cs
@@ -0,0 +1,45 @@
+using Entities.Models;
+using Repositories.Contracts;
+using Services.Contracts;
+
+namespace Services;
+
+public class BookManager : IBookService
+{
+    private readonly IRepositoryManager _manager;
+
+    public BookManager(IRepositoryManager manager)
+    {
+        _manager = manager;
+    }
+
+    public Book CreateOneBook(Book book)
+    {
+        if (book is null)
+            throw new Exception("Kitap nesnesi boş olamaz!");
+
+        _manager.BookRepository.Create(book);
+        _manager.Save();
+        return book;
+    }
+
+    public void DeleteOneBook(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Book> GetAllBooks()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Book GetOneBook(int id, bool trackChanges)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Book UpdateOneBook(int id, Book book)
+    {
+        throw new NotImplementedException();
+    }
+}
