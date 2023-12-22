26b6f2729b540e6319201677ba2dbe9e4667a4ed
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-22 11:34:10 +0300

    RepositoryManager has been defined.

---
+++ api/Repositories/RepositoryManager.cs
@@ -0,0 +1,21 @@
+﻿using Repositories.Contracts;
+
+namespace Repositories;
+
+public class RepositoryManager : IRepositoryManager
+{
+    private readonly IBookRepository _bookRepository;
+
+    public RepositoryManager(IBookRepository bookRepository)
+    {
+        _bookRepository = bookRepository;
+    }
+
+    public IBookRepository BookRepository =>
+        _bookRepository;
+
+    public void Save()
+    {
+        throw new NotImplementedException();
+    }
+}
