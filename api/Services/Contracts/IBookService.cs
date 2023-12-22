e7c4ab10eeaf5bdfb3c42a9f4f0e2cd341d19d5d
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-22 14:06:38 +0300

    Services project has been updated.

--- api/Services/Contracts/IBookService.cs
+++ api/Services/Contracts/IBookService.cs
@@ -5,4 +5,8 @@ namespace Services.Contracts;
 public interface IBookService
 {
     IEnumerable<Book> GetAllBooks();
+    Book GetOneBook(int id, bool trackChanges);
+    Book CreateOneBook(Book book);
+    Book UpdateOneBook(int id, Book book);
+    void DeleteOneBook(int id);
 }
